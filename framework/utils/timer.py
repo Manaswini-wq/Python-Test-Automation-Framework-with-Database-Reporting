45c2422f2a9c7b5edea6249b417a78a1f576929f
Manaswini-wq <manasu2003@gmail.com>
2026-09-17 01:16:32 +0530

    Add Timer utility for test duration measurement
    Implement a Timer class for measuring execution duration.

--- framework/utils/timer.py
+++ framework/utils/timer.py
@@ -1,1 +1,26 @@
+"""
+Simple timer utility for measuring test execution duration.
+"""
+import time
 
+
+class Timer:
+    def __init__(self):
+        self._start: float = 0
+        self._end: float = 0
+        self._running: bool = False
+
+    def start(self):
+        self._start = time.perf_counter()
+        self._running = True
+
+    def stop(self) -> float:
+        self._end = time.perf_counter()
+        self._running = False
+        return self.elapsed
+
+    @property
+    def elapsed(self) -> float:
+        if self._running:
+            return time.perf_counter() - self._start
+        return self._end - self._start
